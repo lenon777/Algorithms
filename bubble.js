3ee10e1b3a76ca63036abfb0399c2d7795cef1c2
lenon777 <amytsak90@gmail.com>
2019-07-22 18:36:31 +0300

    format

--- bubble.js
+++ bubble.js
@@ -1,15 +1,16 @@
-function bubbleSort(arr){
+function bubbleSort(arr) {
     var current;
-    for(var i = 0;i<arr.length;i++){
-        for(var j = 0;j<arr.length;j++){
-            if(arr[j]>arr[j+1]){
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
                 current = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = current;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = current;
             }
         }
     }
     return arr;
 }
 
-var arr = [4,1,2,4,7,9,0,3];
+var arr = [4, 1, 2, 4, 7, 9, 0, 3];
+bubbleSort(arr);
