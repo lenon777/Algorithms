15caf75ef74aa2fca945ba21c32f3096f3f2efef
Andrii Mytsak <40431179+lenon777@users.noreply.github.com>
2019-07-23 11:48:47 +0300

    Add files via upload

---
+++ binary.js
@@ -0,0 +1,22 @@
+function binarySearch(arr, item) {
+    var first = 0;
+    var last = arr.length - 1;
+    var index = -1;
+    var isElement = false;
+    var middle;
+
+    while (isElement === false && first <= last) {
+        middle = Math.floor((first + last) / 2);
+        if (arr[middle] == item) {
+            index = middle;
+            isElement = true;
+        } else if (arr[middle] > item) {
+            last = middle - 1;
+        } else {
+            first = middle + 1;
+        }
+    }
+    return index;
+}
+var arr = [1, 2, 4, 6, 8, 9, 7];
+binarySearch(2, arr)
